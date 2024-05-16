a4438ee7dd9b0c01c4b80a3cbfd9661a4592ffb8
José Gabriel <gabriel-pontes09@hotmail.com>
2024-05-16 19:40:55 -0300

    refactor: update vercel.json to use absolute path for main.js

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({}); // enable cors, only for development
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
